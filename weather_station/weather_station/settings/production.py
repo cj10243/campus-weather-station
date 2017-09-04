c6a3e4d961516c2b64ade66521b31fa87326b8f8
Jonathan <jonathan950400@gmail.com>
2017-09-05 00:11:44 +0800

    update

--- weather_station/weather_station/settings/production.py
+++ weather_station/weather_station/settings/production.py
@@ -2,8 +2,8 @@ from .base import *
 
 DEBUG = False
 
-SECRET_KEY = get_env_var('DJANGO_WEATHER_STATION_SECRET_KEY')
-
+#SECRET_KEY = get_env_var('DJANGO_WEATHER_STATION_SECRET_KEY')
+SECRET_KEY = 'ke67!frybp8=#&&n33m3l5tpc)#t9#rv@784z9)t&0crhl55@^'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
