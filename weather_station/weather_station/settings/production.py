7fbd2f578d280fe67cf40ae3f914ad4fa84b936a
Jonathan <jonathan950400@gmail.com>
2017-09-04 22:53:15 +0800

    add db infor

--- weather_station/weather_station/settings/production.py
+++ weather_station/weather_station/settings/production.py
@@ -6,12 +6,12 @@ SECRET_KEY = get_env_var('DJANGO_WEATHER_STATION_SECRET_KEY')
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': 'weatherstation',
         'USER': get_env_var('DJANGO_WEATHER_STATION_DATABASE_DEFAULT_USER'),
         'PASSWORD': get_env_var('DJANGO_WEATHER_STATION_DATABASE_DEFAULT_PASSWORD'),
-        'HOST': 'localhost',
-        'PORT': '',
+        'HOST': get_env_var('DJANGO_WEATHER_STATION_DATABASE_DEFAULT_HOST'),
+        'PORT': '3306',
     },
 }
 
