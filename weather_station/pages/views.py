ff8a6df9a8d04b15bf4ac12447397c823c9655f3
Jonathan <jonathan950400@gmail.com>
2017-09-07 21:16:14 +0800

    fix some bugs

--- weather_station/pages/views.py
+++ weather_station/pages/views.py
@@ -3,8 +3,7 @@ from  weather.models import Weather
 # Create your views here.
 def home(request):
     weather = Weather.objects.all()
-
-    return render(request, 'pages/home.html',{'weather': weather})
-def status(request):
-    weather = Weather.objects.all()
     return render(request, 'pages/home.html', {'weather': weather})
+
+def about(request):
+    return render(request,'pages/about.html')
