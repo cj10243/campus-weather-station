d26572ab6947e94e18b667702a47c52486fbc09b
Jonathan Cheng <jonathan950400@gmail.com>
2017-09-23 15:26:57 +0800

    add the environment variable to the host

--- weather_station/gunicorn.conf.py
+++ weather_station/gunicorn.conf.py
@@ -2,9 +2,9 @@
 # /usr/bin/python3
 import os
 
-bind = '203.72.63.200:8000'
+bind = '{}:8000'.format(os.environ['DJANGO_WEATHER_STATION_HOST'])
 worders = (os.sysconf('SC_NPROCESSORS_ONLN') * 2) + 1
 loglevel = 'error'
-command = '/home/nhcc/venv/weather_station/bin/gunicorn'
-pythonpath = '/home/nhcc/campus-weather-station/weather_station'
+command = "WTR_VENV/gunicorn"
+pythonpath = "$PROJECT/weather_station"
 
