24f3ab0d7f2b4ee19a786cbff05a6c4f86ae929b
Jonathan <jonathan950400@gmail.com>
2017-09-26 04:56:59 +0800

    add the chartviews

--- weather_station/pages/views.py
+++ weather_station/pages/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from  weather.models import Weather
 from django.views.generic.base import TemplateView
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 class HomeView(TemplateView):
     template_name = "pages/home.html"
@@ -17,3 +19,25 @@ class StatusView(TemplateView):
 '''
 class AboutView(TemplateView):
     template_name = "pages/about.html"
+
+class ChartData(APIView):
+    authentication_classes = []
+    permission_classes = []
+
+    def get(self, request, format=None):
+        temperature = [Weather.objects.order_by('-time')[i].temperature for i in range(287)]
+        humidity = [Weather.objects.order_by('-time')[i].humidity for i in range(287)]
+        uv = [Weather.objects.order_by('-time')[i].uv for i in range(287)]
+        light = [Weather.objects.order_by('-time')[i].light for i in range(287)]
+        rainfall = [Weather.objects.order_by('-time')[i].rainfall for i in range(287)]
+
+        labels = ["Temperature", "Humidity", "UV", "Light", "RainFall"]
+        data = {
+            "temperature": temperature,
+            "humidity": humidity,
+            "uv": uv,
+            "light": light,
+            "rainfall": rainfall,
+            "labels": labels,
+        }
+        return Response(data,template_name='pages/status.html')
