68fadcc354aa06ec92bb3a354e8ddd661ac2ebaa
Jonathan <jonathan950400@gmail.com>
2017-09-25 12:14:44 +0800

    merge from dev branch

--- weather_station/weather/models.py
+++ weather_station/weather/models.py
@@ -4,11 +4,11 @@ from django.db import models
 
 class Weather(models.Model):
     time = models.DateTimeField()
-    temperature = models.DecimalField(max_digits=3,decimal_places=1)
-    humidity = models.DecimalField(max_digits=3,decimal_places=1)
-    uv = models.IntegerField()
-    light = models.IntegerField()
-    rainfall = models.DecimalField(max_digits=2,decimal_places=1)
+    temperature = models.DecimalField(max_digits=3,decimal_places=1,default=None,null=True)
+    humidity = models.DecimalField(max_digits=3,decimal_places=1,default=None,null=True)
+    uv = models.IntegerField(default=None,null=True)
+    light = models.IntegerField(default=None,null=True)
+    rainfall = models.IntegerField(default=None,null=True)
 
     def __str__(self):
         weather = '{0.time} {0.temperature} {0.humidity} {0.uv} {0.light} {0.rainfall}'
@@ -16,6 +16,7 @@ class Weather(models.Model):
 
 
     #之前沒有加str()出現TypeError: __repr__ returned non-string (type datetime.datetime)
+
 
 
 
