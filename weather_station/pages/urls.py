f882662b9d14858cfbf8940b96a8c3eda44ba1b8
Jonathan <jonathan950400@gmail.com>
2017-09-30 22:39:35 +0800

    merge from master

--- weather_station/pages/urls.py
+++ weather_station/pages/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
-from pages.views import HomeView,StatusView,AboutView
+from pages.views import HomeView,StatusView,AboutView,ChartData
 from . import views
 
 urlpatterns = [
     url(r'^$', views.HomeView.as_view(), name='home'),
     url(r'^status$', views.StatusView.as_view(), name='status'),
     url(r'^about$', views.AboutView.as_view(), name='about'),
+    url(r'^api/chart/data/$', ChartData.as_view()),
 ]
--- weather_station/pages/views.py
+++ weather_station/pages/views.py
@@ -1,19 +1,52 @@
 from django.shortcuts import render
 from  weather.models import Weather
+from school.models import School
 from django.views.generic.base import TemplateView
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 class HomeView(TemplateView):
     template_name = "pages/home.html"
+
+class ChartData(APIView):
+    authentication_classes = []
+    permission_classes = []
+
+    def get(self, request, format=None):
+
+        temperature = [Weather.objects.order_by('-time')[i].temperature for i in range(287)][::-1]
+        print(temperature)
+        humidity = [Weather.objects.order_by('-time')[i].humidity for i in range(287)][::-1]
+        uv = [Weather.objects.order_by('-time')[i].uv for i in range(287)][::-1]
+        light = [Weather.objects.order_by('-time')[i].light for i in range(287)][::-1]
+        rainfall = [Weather.objects.order_by('-time')[i].rainfall for i in range(287)][::-1]
+        labels = ["Temperature", "Humidity", "UV", "Light", "RainFall"]
+        data = {
+            "temperature": temperature,
+            "humidity": humidity,
+            "uv": uv,
+            "light": light,
+            "rainfall": rainfall,
+            "labels": labels,
+        }
+        return Response(data,template_name='pages/status.html')
 
 class StatusView(TemplateView):
     template_name = "pages/status.html"
     def get_context_data(self, **kwargs):
         context = super(StatusView, self).get_context_data(**kwargs)
-        context['weathers'] = Weather.objects.all()
+        schools = School.objects.all()
+        context['weathers'] =  [Weather.objects.filter(school=schools[i]).order_by('-time')[0] for i in range(0,len(schools))]
+
+
+        # context['chart'] = {"renderTo":context['chartID'], "type": "line", "height": 500,}
+        # context['series'] = [{"name": 'Label1', "data": [1,2,3]}, {"name": 'Label2', "data": [4, 5, 6]}]
+        # context['title'] = {"text": 'My Title'}
+        # context['xAxis'] = {"categories": ['xAxis Data1', 'xAxis Data2', 'xAxis Data3']}
+        # context['yAxis'] = {"title": {"text": 'yAxis Label'}}
         return context
-    '''
-    def weathers(self):
-        return Weather.objects.order_by().all()[0]
-'''
+
 class AboutView(TemplateView):
     template_name = "pages/about.html"
+
+
