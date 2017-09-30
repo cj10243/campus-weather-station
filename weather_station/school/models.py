9397b75a6d8c7ff5e17b9ac248f8ea030ebe0628
Jonathan <jonathan950400@gmail.com>
2017-09-30 21:20:12 +0800

    update page

--- weather_station/school/models.py
+++ weather_station/school/models.py
@@ -1,17 +1,18 @@
 from django.db import models
-from weather.models import Weather
 # Create your models here.
-
 class School(models.Model):
     name = models.CharField(max_length=10,null=False)
     eng_name = models.CharField(max_length=50,null=False)
-    school_id = models.CharField(max_length=8,null=False)
-    weather = models.ForeignKey(Weather,related_name="school",on_delete=models.CASCADE)
+    school_abbreviation= models.CharField(max_length=8,null=False)
+
 
     def __str__(self):
-        school = '{0.name} {0.eng_name} {0.school_id}'
+        school = '{0.name} {0.eng_name} {0.school_abbreviation}'
         return school.format(self)
 
+    class Meta:
+        db_table='school'
     #[School.objects.filter(id=i).update(weather_id=i) for i in range(1,2837)]
     #[School(name="臺北市立內湖高中",eng_name="Taipei Municipal Nei-Hu Senior High School",weather_id=i).save() for i in range(1,2837)]
     #[School(name="國立華僑高中",eng_name="National Overseas Chinese Senior High School",weather_id=i).save() for i in range(1,2837)]
+#[Weather.objects.filter(id=i).update(school=a) for i in range(1,2837)]
