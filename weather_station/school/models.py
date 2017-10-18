80f60b6beba2e1ecb460755fe909ea301ffa1069
Jonathan <jonathan950400@gmail.com>
2017-10-19 07:30:15 +0800

    update model

--- weather_station/school/models.py
+++ weather_station/school/models.py
@@ -16,3 +16,9 @@ class School(models.Model):
     #[School(name="臺北市立內湖高中",eng_name="Taipei Municipal Nei-Hu Senior High School",weather_id=i).save() for i in range(1,2837)]
     #[School(name="國立華僑高中",eng_name="National Overseas Chinese Senior High School",weather_id=i).save() for i in range(1,2837)]
 #[Weather.objects.filter(id=i).update(school=a) for i in range(1,2837)]
+'''
+School(1,"臺北市立內湖高級中學","Taipei Municipal Neihu High School","nhsh").save()
+School(2,"國立華僑高級中學","National Overseas Chinese Senior High School","nocsh").save()
+School(3,"臺北市立中山女高","Taipei Municipal Zhongshan Girls High School","csghs").save()
+School(4,"臺北市立景美女中","Taipei Municipal Jingmei Girls' High School","cmgsh").save()
+'''
