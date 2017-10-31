ff5230cbf6920ba6e60a50ee6ea9557cc12320c8
Jonathan Cheng <jonathan950400@gmail.com>
2017-11-01 01:22:31 +0800

    modify production

--- weather_station/weather_station/settings/production.py
+++ weather_station/weather_station/settings/production.py
@@ -7,7 +7,7 @@ SECRET_KEY = get_env_var('DJANGO_WEATHER_STATION_SECRET_KEY')
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'weatherstation_test',
+        'NAME': 'weather_station',
         'USER': get_env_var('DJANGO_WEATHER_STATION_DATABASE_DEFAULT_USER'),
         'PASSWORD': get_env_var('DJANGO_WEATHER_STATION_DATABASE_DEFAULT_PASSWORD'),
         'HOST': get_env_var('DJANGO_WEATHER_STATION_DATABASE_DEFAULT_HOST'),
